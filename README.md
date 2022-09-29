c53671eee7893c1d60f9b165883f1c644bfcd0cf
AliSAlfaili <112570421+AliSAlfaili@users.noreply.github.com>
2022-09-29 17:26:17 +0300

    Final_Project_done

--- README.md
+++ README.md
@@ -3,31 +3,19 @@
 <div dir="rtl">
 
 ### اسم الطالب
-
+Ali Sami Alfaili
 
 ### اسم المشروع
-
+Surf the Ocean
 
 # شرح الفكرة
 المشكلة والحلول المطروحة إن وجدت
-
-
+Explore the oceans my a map, when clicking on any of the oceans it will take you to it and show you how it look like, and there are two circles in the center one take you with the non-frozen oceans the other take you to the frozen oceans.
 # الأعمال المستقبلية
-
+Nothing i tried to make it as simple and fun as possible
 
 </div>
 
-> Fill out project information in English ⬇️
-### Student Name
-
-
-### Project Name
-
-# Idea
-Problem and solutions if found 
-
-
-# Future Work 
 
 
 
---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("tryclick").onclick = function () {
+    location.href = "./indianO.html";
+};
+document.getElementById("clickhere").onclick = function () {
+    location.href = "./southO.html";
+};
+document.getElementById("tryclickI").onclick = function () {
+    location.href = "./atlanticO.html";
+};
+document.getElementById("clickhereI").onclick = function () {
+    location.href = "./arcticO.html";
+};
+document.getElementById("tryclickII").onclick = function () {
+    location.href = "./atlanticO.html";
+};
+document.getElementById("clickhereII").onclick = function () {
+    location.href = "./arcticO.html";
+};
